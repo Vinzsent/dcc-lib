4787c0156ca22e17f6e4122bc0f823155c000bc0
Vincent Crame <vincentcrame7@gmail.com>
2026-03-31 08:05:09 +0800

    create: books table

---
+++ database/migrations/2026_03_23_100345_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->string('accession_no')->primary();
+            $table->string('title');
+            $table->string('author');
+            $table->string('call_number');
+            $table->enum('status', ['Available', 'Borrowed'])->default('Available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
