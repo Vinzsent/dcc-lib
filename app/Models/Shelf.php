bcb88117939416c8538ab4d66907387aa8042520
Vincent Crame <vincentcrame7@gmail.com>
2026-05-22 09:54:59 +0800

    add: shef

---
+++ app/Models/Shelf.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shelf extends Model
+{
+    protected $fillable = [
+        'shelf_number',
+        'description'
+    ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class, 'shelf_number', 'shelf_number');
+    }
+}
