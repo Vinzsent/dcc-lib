9c1b3ba3a6602d3c4eb2d176b6f4d2545ffef416
Vincent Crame <vincentcrame7@gmail.com>
2026-07-17 09:28:24 +0800

    Add Research navigation link to sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -403,6 +403,16 @@
                 </a>
             </li>
             <li class="nav-item">
+    <a href="{{ route('admin.library.research.index') }}" class="nav-link {{ request()->routeIs('admin.library.research.*') ? 'active' : '' }}">
+        <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+        </svg>
+        <span class="link-text">Research</span>
+    </a>
+</li>
+            <li class="nav-item">
                 <a href="{{ route('admin.library.shelves.index') }}" class="nav-link {{ request()->routeIs('admin.library.shelves.*') ? 'active' : '' }}">
                     <svg class="nav-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
